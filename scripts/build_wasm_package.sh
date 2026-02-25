513ce72d10dbf691af872a52c9ca87e3b40ea4d6
Gordon Smith <gsmith@route8.io>
2026-02-24 23:04:30 -0600

    feat(build): update WASM build process with optimized flags and caching

--- scripts/build_wasm_package.sh
+++ scripts/build_wasm_package.sh
@@ -2,4 +2,4 @@
 
 # look at DeepFilterNet/.github/workflows/build_wasm.yml for enviroment setup
 cd ./libDF/
-wasm-pack build --target no-modules --features wasm
+RUSTFLAGS="-C target-feature=+simd128,+bulk-memory" wasm-pack build --target no-modules --features wasm -- --profile release-lto
