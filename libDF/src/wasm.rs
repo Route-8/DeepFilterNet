358a87c65dcce9a35e3abda02b7cbd1e150349ec
Gordon Smith <gsmith@route8.io>
2026-02-24 22:42:52 -0600

    feat(DFState): refactor DFState struct to include output buffer and update processing methods

--- libDF/src/wasm.rs
+++ libDF/src/wasm.rs
@@ -16,7 +16,10 @@ pub fn init_panic_hook() {
 }
 
 #[wasm_bindgen]
-pub struct DFState(crate::tract::DfTract);
+pub struct DFState {
+    inner: crate::tract::DfTract,
+    output_buf: Array2<f32>,
+}
 
 #[wasm_bindgen]
 impl DFState {
@@ -27,8 +30,11 @@ impl DFState {
         let df_params = DfParams::from_bytes(model_bytes).expect("Could not load model from bytes");
         let m =
             DfTract::new(df_params, &r_params).expect("Could not initialize DeepFilter runtime");
-
-        DFState(m)
+        let hop_size = m.hop_size;
+        DFState {
+            inner: m,
+            output_buf: Array2::zeros((1, hop_size)),
+        }
     }
     fn boxed(self) -> Box<DFState> {
         Box::new(self)
@@ -57,7 +63,7 @@ pub unsafe fn df_create(
 #[wasm_bindgen]
 pub unsafe fn df_get_frame_length(st: *mut DFState) -> usize {
     let state = st.as_mut().expect("Invalid pointer");
-    state.0.hop_size
+    state.inner.hop_size
 }
 
 /// Set DeepFilterNet attenuation limit.
@@ -67,7 +73,7 @@ pub unsafe fn df_get_frame_length(st: *mut DFState) -> usize {
 #[wasm_bindgen]
 pub unsafe fn df_set_atten_lim(st: *mut DFState, lim_db: f32) {
     let state = st.as_mut().expect("Invalid pointer");
-    state.0.set_atten_lim(lim_db)
+    state.inner.set_atten_lim(lim_db)
 }
 
 /// Set DeepFilterNet post filter beta. A beta of 0 disables the post filter.
@@ -77,7 +83,7 @@ pub unsafe fn df_set_atten_lim(st: *mut DFState, lim_db: f32) {
 #[wasm_bindgen]
 pub unsafe fn df_set_post_filter_beta(st: *mut DFState, beta: f32) {
     let state = st.as_mut().expect("Invalid pointer");
-    state.0.set_pf_beta(beta)
+    state.inner.set_pf_beta(beta)
 }
 
 /// Processes a chunk of samples.
@@ -92,10 +98,8 @@ pub unsafe fn df_set_post_filter_beta(st: *mut DFState, beta: f32) {
 #[wasm_bindgen]
 pub unsafe fn df_process_frame(st: *mut DFState, input: &[f32]) -> js_sys::Float32Array {
     let state = st.as_mut().expect("Invalid pointer");
-    let input = ArrayView2::from_shape((1, state.0.hop_size), input).unwrap();
-
-    let mut output = Array2::zeros((1, state.0.hop_size));
-    let output_view = output.view_mut();
-    let _lsnr = state.0.process(input, output_view).expect("Failed to process DF frame");
-    js_sys::Float32Array::from(output.as_slice().unwrap())
+    let input = ArrayView2::from_shape((1, state.inner.hop_size), input).unwrap();
+    let output_view = state.output_buf.view_mut();
+    let _lsnr = state.inner.process(input, output_view).expect("Failed to process DF frame");
+    js_sys::Float32Array::from(state.output_buf.as_slice().unwrap())
 }
